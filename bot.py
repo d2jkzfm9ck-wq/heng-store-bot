b032fc89d3b896087f5263d5847c0e6cc006451e
d2jkzfm9ck-wq <d2jkzfm9ck@privaterelay.appleid.com>
2026-07-31 21:25:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -79,17 +79,6 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("balance", balance))
 app.add_handler(CommandHandler("games", games))
 
-import asyncio
+print("✅ Heng Store Bot is running...")
 
-async def main():
-    print("✅ Heng Store Bot is running...")
-
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-
-    while True:
-        await asyncio.sleep(3600)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+app.run_polling()
